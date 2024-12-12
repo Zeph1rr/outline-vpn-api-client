17d7ccb93887dd5f16e4892c7f82d16539b29bce
zeph1rr <grianton535@gmail.com>
2024-12-12 23:24:54 +0300

    repair tests

--- tests/test_access_key.py
+++ tests/test_access_key.py
@@ -24,7 +24,7 @@ def test_access_key_create(name: str, limit: int, client: OutlineClient):
     if limit:
         assert user.get("dataLimit").get("bytes") == 150000000000
 
-@pytest.mark.parametrize('id, name, limit, is_error', [(4, 'third_test_client', None, False), (3, 'fourth_test_client', 150000000000, False), (3, 'fourth_test_client', 150000000000, True)])
+@pytest.mark.parametrize('id, name, limit, is_error', [(212, 'third_test_client', None, False), (415, 'fourth_test_client', 150000000000, False), (3, 'fourth_test_client', 150000000000, True)])
 def test_access_key_create_with_special_id(id: int, name: str, limit: int, is_error: bool,  client: OutlineClient):
     if is_error:
         with pytest.raises(ResponseNotOkException) as _ex:
@@ -38,16 +38,16 @@ def test_access_key_create_with_special_id(id: int, name: str, limit: int, is_er
             assert user.get("dataLimit").get("bytes") == 150000000000
 
 def test_access_key_rename(client: OutlineClient):
-    assert client.access_keys.rename(3, "renamed_fourth_test_client")
-    assert client.access_keys.get(3).get("name") == "renamed_fourth_test_client"
+    assert client.access_keys.rename(415, "renamed_fourth_test_client")
+    assert client.access_keys.get(415).get("name") == "renamed_fourth_test_client"
 
 def test_access_key_change_data_limit(client: OutlineClient):
-    assert client.access_keys.change_data_limit(4, 150000000000)
-    assert client.access_keys.get(4).get("dataLimit").get("bytes") == 150000000000
+    assert client.access_keys.change_data_limit(212, 150000000000)
+    assert client.access_keys.get(212).get("dataLimit").get("bytes") == 150000000000
 
 def test_access_key_remove_data_limit(client: OutlineClient):
-    assert client.access_keys.remove_data_limit(4)
-    assert client.access_keys.get(4).get("dataLimit") is None
+    assert client.access_keys.remove_data_limit(212)
+    assert client.access_keys.get(212).get("dataLimit") is None
 
 
 def test_access_key_delete(client: OutlineClient):
