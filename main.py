ea2d1f23892f446cb39d30c441ea3a233b8b6195
zeph1rr <grianton535@gmail.com>
2024-12-21 17:25:50 +0300

    added async client

--- main.py
+++
@@ -1,8 +0,0 @@
-from outline_vpn_api_client import OutlineClient
-
-def main():
-    client = OutlineClient("https://146.185.218.101:41023/wH9tEDXAA-LY0qW8FQHyPA")
-    client.access_keys.create_with_special_id(0, "special_key")
-
-if __name__ == "__main__":
-    main()
